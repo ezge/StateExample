91b8ada58fd04f4678c133d3d6b09b5496678a79
ezge <ezge.rea@gmail.com>
2025-03-17 12:41:36 +0100

    Initial commit

---
+++ app/src/main/java/com/assessment/stateexample/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.assessment.stateexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.assessment.stateexample.ui.theme.StateExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StateExampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DemoScreen(modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DemoScreen(modifier: Modifier = Modifier) {
+    var textState by remember { mutableStateOf("") }
+    val onTextChange = { text: String -> textState = text }
+    Column (horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier.fillMaxSize()){
+            MyTextField(text = textState, onTextChange = onTextChange)
+    }
+}
+
+@Composable
+fun MyTextField(text: String, onTextChange: (String) -> Unit){
+
+        TextField(value = text,
+                  onValueChange = onTextChange)
+        FunctionA()
+}
+
+@Composable
+fun FunctionA(){
+    var switchState by remember { mutableStateOf(true) }
+    val onSwitchChange = { value: Boolean -> switchState = value }
+
+    FunctionB(switchState = switchState,
+              onSwitchChange = onSwitchChange)
+
+}
+
+@Composable
+fun FunctionB(switchState: Boolean, onSwitchChange: (Boolean) -> Unit){
+    Switch(checked = switchState,
+           onCheckedChange = onSwitchChange)
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StateExampleTheme {
+        DemoScreen()
+    }
+}
